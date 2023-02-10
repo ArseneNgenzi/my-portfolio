a813c28b618a9fca199a67edc19e71f9a3751901
ArseneNgenzi <arsenengenzi@gmail.com>
2023-02-10 02:50:49 +0200

    fixed typos

--- components/WorkHistory.tsx
+++ components/WorkHistory.tsx
@@ -62,7 +62,7 @@ export interface WorkProps {
                 <SingleWork work={job} key={id} />
                     {/* <h1 key={id} style={{color:'white'}}>{job.company}</h1> */}
                 <div className='bg-teal- flex justify-center pt-6'>
-                    <a href="https://resume.io/r/IEF94vgYO" target={'_blank'} className='text-teal-500 underline capitalize hover:no-underline'>Click here to see resume</a>
+                    <a href="https://resume.io/r/IEF94vgYO" target={'blank'} className='text-teal-500 underline capitalize hover:no-underline'>Click here to see resume</a>
                 </div>
                 </>
             )
